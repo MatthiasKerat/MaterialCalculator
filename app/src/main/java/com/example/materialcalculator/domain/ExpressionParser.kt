204d4019da2c2cae78bb46feab19ce6b68f268bb
Matzessi <MatthiasKerat@web.de>
2022-08-30 15:02:33 +0200

    Added ExpressionParser.kt

---
+++ app/src/main/java/com/example/materialcalculator/domain/ExpressionParser.kt
@@ -0,0 +1,56 @@
+package com.example.materialcalculator.domain
+
+class ExpressionParser(
+    private val calcuation:String
+) {
+
+    fun parse():List<ExpressionPart>{
+        val result = mutableListOf<ExpressionPart>()
+
+        var i = 0
+        while(i<calcuation.length){
+            val curChar = calcuation[i]
+            when{
+                curChar in operationSymbols -> {
+                    result.add(
+                        ExpressionPart.Op(operationFromSymbol(curChar))
+                    )
+                }
+                curChar.isDigit() -> {
+                    i = parseNumber(i, result)
+                    continue
+                }
+                curChar in "()"->{
+                    parseParentheses(curChar, result)
+                }
+            }
+            i++
+        }
+        return result
+    }
+
+    private fun parseNumber(startingIndex: Int, result: MutableList<ExpressionPart>): Int {
+        var i = startingIndex
+        val numberAsString = buildString {
+            while(i < calcuation.length && calcuation[i] in "0123456789."){
+                append(calcuation[i])
+                i++
+            }
+        }
+        result.add(ExpressionPart.Number(numberAsString.toDouble()))
+        return i
+    }
+
+    private fun parseParentheses(curChar: Char, result: MutableList<ExpressionPart>){
+        result.add(
+            ExpressionPart.Parentheses(
+                type = when(curChar){
+                    '(' -> ParenthesesType.Opening
+                    ')' -> ParenthesesType.Closing
+                    else -> throw IllegalArgumentException("Invalid parentheses type")
+                }
+            )
+        )
+    }
+
+}
